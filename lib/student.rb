332cbbdb479678e624529b272dc003fc2d2e6dc8
Joshua Palacios <palacios.joshua@gmail.com>
2017-11-28 18:20:29 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -60,7 +60,7 @@ class Student
   end
 
   def self.first_X_students_in_grade_10(x)
-    sql = "SELECT * FROM students WHERE grade = 10 AND id < ?"
+    sql = "SELECT * FROM students WHERE grade = 10 AND id <= ?"
     DB[:conn].execute(sql, x)
   end
 end
