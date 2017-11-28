08a1b2c3d7d69a2106ad28b0b13c867346451e65
Joshua Palacios <palacios.joshua@gmail.com>
2017-11-28 18:09:49 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -16,22 +16,22 @@ class Student
 
   def self.find_by_name(name)
     sql <<-SQL
-      SELECT * 
+      SELECT *
       FROM students
       WHERE name = ?
     SQL
     DB[:conn].execute(sql, name).map {|row| self.new_from_db(row)}
   end
-  
+
   def save
     sql = <<-SQL
-      INSERT INTO students (name, grade) 
+      INSERT INTO students (name, grade)
       VALUES (?, ?)
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
   end
-  
+
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
